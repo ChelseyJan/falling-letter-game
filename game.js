524142df4569d5675c6157e4343210d3acb6e0a7
ChelseyJan <chelseyjan@gmail.com>
2020-02-22 14:20:41 -0800

    add color randomizer

--- game.js
+++ game.js
@@ -16,7 +16,7 @@ function GenerateLetters(count)
   let myVar = document.getElementById("letterContainer");
   for (let i = 0; i < count; i++)
   {
-    arr.push(  {
+    arr.push({
       text: "",
       timeout: 5,
       visibility: true,
@@ -26,6 +26,7 @@ function GenerateLetters(count)
     arr[i].element = document.createElement("div");
     arr[i].element.id = "letter-" + i;
     arr[i].element.textContent = "A";
+    RandomColor(arr[i].element);
     myVar.appendChild(arr[i].element);
   }
   return arr;
@@ -64,50 +65,44 @@ function StartCountDown()
 function StartGame() 
 {
   console.log("start game!");
-  GameLoop(10);
+  GameLoop(100);
 }
 
+function RandomColor(element) 
+{
+  var colorArray = [
+    "#ee8572",
+    "#f0b182",
+    "#f9d189"
+  ];
+
+  var randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+  element.style.color = randomColor;
+}
 
 function GameLoop(gameLength)
 {
-  count=0;
-  let game= window.setInterval(()=>{
-     if(letterPool.length)
-     {
-       activeLetters.push(letterPool.shift());
+  count = 0;
+  let game = window.setInterval(() =>
+  {
+    if (letterPool.length)
+    {
+      activeLetters.push(letterPool.shift());
       // console.log(letterPool.length);
-       let letter=activeLetters[activeLetters.length-1];
- 
-       letter.element.className+= " letter-hide";
-       letter.timeout= window.setTimeout(()=>{
-       letter.element.className= "";
-       letterPool.push(activeLetters.shift());
-       },5000);
+      let letter = activeLetters[activeLetters.length - 1];
+      letter.element.className += " letter-hide";
+      letter.timeout = window.setTimeout(() =>
+      {
+        letter.element.className = "";
+        RandomColor(letter.element);
+        letterPool.push(activeLetters.shift());
+      }, 5000);
       // activeLetters.timeout();  
-     }
-     count++;
-     if(count>=gameLength)
-     {
+    }
+    count++;
+    if (count >= gameLength)
+    {
       window.clearInterval(game);
-     }
-  },1000);
+    }
+  }, 1000);
 }
-
-
-// function TrackLetterPosition (element){
-//     var newTimer = window.setInterval(() => {
-//         MoveDown(element);
-//     }, 1000);
-//     console.log("TrackLetterPosition function complete");
-// };
-//make an interval timer thing that tracks how far/long the element has traveled using fall distance/speed ratio
-//once it goes too far - make it disappear + add to the counter for misses
-//also - if the correct key is hit, exit + add a counter for good
-
-// function MoveDown(element) 
-// {
-//   console.log("Inside MoveDown function");
-//   var elementStyle = window.getComputedStyle(element);
-//   var topValue = elementStyle.getPropertyValue("top").replace("px", "");
-//   element.style.top = Number(topValue) + fallingSpeed + "px";
-// }
